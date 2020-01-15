69a1f74b6361dd192289af97642f2766bbefd3ab
gayathribuddhika <wg.gayathri@gmail.com>
2020-01-15 17:13:22 +0530

    updated

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,18 +3,31 @@ const mongodb = require ('mongodb');
 
 const router = express.Router();
 
+
 //get posts
-router.get('/', (req, res) => {
-    res.send('Hello');
+router.get('/', async (req, res) => {
+    
+    const posts = await loadPostsCollection()
+    res.send(await posts.find({}).toArray())
+    
 });
 
 //add posts
+router.post('/', async(req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createAt: new Date()
+    });
+    res.sendStatus(201).send();
+});
 
 //delete posts
 
-async function loadPOstsCollection(){
-    const client = await mongodb.MongoClientconnect('mongodb+srv://gayathri_123:gayathri123@cluster0-gfcor.mongodb.net/test', {
+async function loadPostsCollection(){
+    const client = await mongodb.MongoClient.connect('mongodb+srv://gayathri_123:gayathri123@cluster0-gfcor.mongodb.net/test', {
         useNewUrlParser: true
-    })
+    });
+    return client.db('sample_vue').collection('posts');
 }
 module.exports = router;
