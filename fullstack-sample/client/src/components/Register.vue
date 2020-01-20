34b72a2f247e9838490789162de01f11b7591714
gayathribuddhika <wg.gayathri@gmail.com>
2020-01-20 23:23:06 +0530

    changed

--- fullstack-sample/client/src/components/Register.vue
+++ fullstack-sample/client/src/components/Register.vue
@@ -1,3 +1,39 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <form action="">
+        <label for="">email</label>
+        <input type="email" name ="email" v-model="email"/><br><br>
+        <label for="">password</label>
+        <input type="password" name="password" v-model="password"><br><br>
+        <button @click="register">Register</button>
+    </form>
+  </div>
+</template>
 
+<script>
+import authentication from '@services/authentication';
 
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await authentication.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
 
+</script>
+
+<style scoped>
+
+</style>
