35432b1bd0ddb831fdc6f32f6105dc0fbbc86fac
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-01-24 23:42:02 +0530

    changed

---
+++ backend/client/src/PostService.js
@@ -0,0 +1,35 @@
+import axios from axios;
+
+const url = 'http://localhost:8085/api/posts';
+
+class PostService {
+    //get posts
+    static getPosts() {
+        return new Promise(async (resolve, reject) => {
+            try{
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createAt: new Data(post.createAt)
+                    }))
+                );
+            } catch(err) {
+                reject(err);
+            }
+        });
+    }
+
+    //create post
+    static insertPost(text) {
+        return axios.post(url, {
+            text
+        });
+    }
+
+    //delete post
+    static deletePost(id) {
+        return axios.delete(`${url}${id}`);
+    }
+}
