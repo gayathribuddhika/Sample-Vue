07fbbf9c41ef0560d253df54011bf4465614a6b2
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-02-21 11:10:34 +0530

    add a new component

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,39 +1,14 @@
 <template>
-  <div id = "app">
-    @_{{user.username}} - {{fullName}}
-    <strong>Followers: {{followers}}</strong>
-    <button @click="followUsers">Follow</button>
+  <div id="app">
+    <userProfile/>
   </div>
-  
 </template>
 
 <script>
-
+import userProfile from './components/userProfile';
 export default {
-  name: 'App',
-  data () {
-    return {
-      followers: 0,
-      user: {
-        username: "gayathribuddhika",
-        firstName: "Gayathri",
-        lastName: "Buddhika",
-        email: "wg.gayathri@gmail.com",
-        isAdmin: true
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    }
-  },
-  methods: {
-    followUsers() {
-      this.followers++;
-    }
-  }
-  
+  name: "App",
+  components: {userProfile}
 }
 </script>
 
---
+++ vue-tutorial/src/components/userProfile.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile_user-panel">
+        <h1 class="user-profile_username">@{{this.user.username}}</h1>
+        <div class= "user-profile_admin-badge"> 
+            Admin
+        </div>
+        <div id="user-profile_follower-count">
+            <strong>followers: </strong> {{followers}}
+        </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'userProfile',
+  data () {
+    return {
+      followers: 0,
+      user: {
+        id:1,
+        username: "_gayathribuddhika",
+        firstName: "Gayathri",
+        lastName: "Buddhika",
+        email: "wg.gayathri@gmail.com",
+        isAdmin: true
+      }
+    }
+  },
+
+  watch: {
+    followUsers(newFollowerCount, oldFollowerCount) {
+      if(oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    }
+  },
+
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+
+  methods: {
+    followUsers() {
+      this.followers++;
+    }
+  },
+
+  mounted() {
+    this.followUsers();
+  }
+  
+}
+</script>
+
+<style>
+.user-profile {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    padding: 50px 5%;
+}
+
+.user-profile_user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+}
+.user-profile_admin-badge {
+    background: rebeccapurple;
+    color: white;
+    border-radius: 5px;
+    margin-right: auto;
+    padding: 0 10px;
+    font-weight: bold;
+}
+
+h1 {
+    margin: 0;
+}
+</style>
